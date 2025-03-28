6af50422ac46b62a6506e825c8f1723754df7f18
Andreas Voss <andreas@anvo.dk>
2025-03-28 10:03:34 +0100

    misc

--- dotnet/start.sh
+++ dotnet/start.sh
@@ -6,5 +6,6 @@ distrobox create --additional-packages "systemd docker" --name dotnet-dev --init
 # How much of this can I get into the image?
 # sudo usermod -aG docker $USER
 # sudo systemctl enable --now docker
-# distrobox-export --app rider
+# distrobox-export --app rider -el "none"
+# distrobox-export --app chromium -el "for distrobox network"
 # wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
